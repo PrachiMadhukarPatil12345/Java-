733c57a05cc21b4a3c7ba2a9586b32499db9a0da
PrachiMadhukarPatil12345 <88551270+PrachiMadhukarPatil12345@users.noreply.github.com>
2023-10-03 17:08:47 +0530

    Add files via upload

---
+++ Autoboxing.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Autoboxing
+{
+	public static void main(String args[])
+	{
+		int x=5;
+		float y=3.14F;
+		long z=600000;
+	
+	//Autoboxing
+	Integer intobj=x;
+	Float floatobj=y;
+	Long longobj=z;
+	
+	System.out.println(intobj);
+	System.out.println(floatobj);
+	System.out.println(longobj);
+	
+	}
+}
---
+++ Unboxing.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Unboxing
+{
+	public static void main(String args[])
+	{
+		Integer a=500;
+		//Float b=2.5;
+		Character c = 'P';
+		Double d= 55.5555;
+		Short s = 22;
+		//String st= "Prachi";
+		
+		
+		int x=a;
+		//float y=b;
+		char z=c;
+		double r=d;
+		short q=s;
+		//string str= st;
+
+System.out.println(x);
+//System.out.println(y);		
+System.out.println(z);
+//System.out.println(str);
+System.out.println(q);
+	}
+}	
+		
